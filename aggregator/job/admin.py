9b4ede53e5d186fd2e476fb54953481f701db104
2Dkun1355 <oliynikbogdan.ua@gmail.com>
2024-08-21 17:33:54 +0300

    Create models 3.2, 3.3, 3.5

--- aggregator/job/admin.py
+++ aggregator/job/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import RawVacancy, Vacancy
 
-# Register your models here.
+
+class RawVacancyAdmin(admin.ModelAdmin):
+    list_display = ['url', 'data']
+
+
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ['source', 'programming_language','level_need', 'salary', 'location', 'is_remote']
+    list_filter = ['source', 'programming_language','level_need', 'salary', 'location', 'is_remote',
+                   'years_need', 'english_lvl', 'update_date']
+
+
+admin.site.register(RawVacancy,RawVacancyAdmin)
+admin.site.register(Vacancy,VacancyAdmin)
--- aggregator/job/models.py
+++ aggregator/job/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class RawVacancy(models.Model):
+    url = models.URLField()
+    data = models.TextField()
+
+
+class Vacancy(models.Model):
+    source = models.CharField(max_length=32, null=True, blank=True)
+    url = models.URLField()
+    raw_data = models.OneToOneField(
+        to=RawVacancy,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    description = models.TextField(null=True, blank=True)
+    programming_language = models.CharField(max_length=32, null=True, blank=True)
+    salary = models.IntegerField(null=True, blank=True)
+    location = models.TextField(null=True, blank=True)
+    is_remote = models.BooleanField(null=True, blank=True)
+    level_need = models.CharField(max_length=32, null=True, blank=True)
+    years_need = models.IntegerField(null=True, blank=True)
+    skills = models.TextField(null=True, blank=True)
+    english_lvl = models.CharField(max_length=32, null=True, blank=True)
+    created_data = models.DateField(null=True, blank=True)
+    parsing_data = models.DateField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
+
